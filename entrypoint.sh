faba2dc45bd13800ad4e2d22725033222f3d3044
gustavokmm <gustavo.uoft@gmail.com>
2023-02-06 14:53:45 -0300

    Tyk

---
+++ entrypoint.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+
+# Mapping to Heroku port
+export TYK_GW_LISTENPORT=$PORT
+
+# Set the delimiter
+IFS='@'
+
+#Read the split words into an array based on space delimiter
+read -a redisInfo <<< "$REDISCLOUD_URL"
+
+# Set delimiter
+IFS=':'
+
+#Read the split words into an array based on space delimiter
+read -a redisCredentials <<< "${redisInfo[0]}"
+read -a redisAddress <<< "${redisInfo[1]}"
+
+redisUsername=${redisCredentials[1]:2}
+redisPassword=${redisCredentials[2]}
+
+export TYK_GW_STORAGE_USERNAME=$redisUsername
+export TYK_GW_STORAGE_PASSWORD=$redisPassword
+export TYK_GW_STORAGE_HOST=${redisAddress[0]}
+export TYK_GW_STORAGE_PORT=${redisAddress[1]}
+
+env
